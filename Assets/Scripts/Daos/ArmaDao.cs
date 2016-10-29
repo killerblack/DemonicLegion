146c758396e6658e2901a261525136163cdd9c47
Orlando Valencia <orlando.proyectox@hotmail.com>
2016-10-28 23:00:54 -0500

    Folders structural changes in project

--- Assets/Scripts/Daos/ArmaDao.cs
+++
@@ -1,9 +0,0 @@
-﻿using Entities;
-using System.Collections.Generic;
-
-interface ArmaDao{
-	Arma getArmaById(int id);
-	Arma getArmaByNombre(string nombre);
-	List<Arma> getAllArmas();
-	int getNumberOfArmas();
-}
